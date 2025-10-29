ec2879a9ca2cd6079c133e2657dea1a365a90e09
geancarlos-brito <grochabrito@gmail.com>
2025-10-29 10:18:28 -0300

    Atualizando a NavBar e Adicionando logo temporaria

--- src/layouts/layoutDefault.vue
+++ src/layouts/layoutDefault.vue
@@ -1,12 +1,36 @@
 <template>
-  <div>
-    Layout
-    <RouterView />
+  <!-- Layout principal -->
+  <div class="flex">
+    <!-- Sidebar (Menu Lateral) -->
+    <div class="w-64" v-show="sidebarAberto">
+      <logo />
+      <menu-lateral />
+    </div>
+
+    <!-- Conteúdo Principal -->
+    <div class="flex flex-col flex-1 overflow-hidden">
+      <!-- Navbar Superior -->
+      <nav-bar v-model:estaAberto="sidebarAberto" />
+
+      <!-- Área de Conteúdo (Rotas) -->
+      <main class="flex-1 overflow-y-auto p-6">
+        <router-view></router-view>
+        <!-- Em React/Vue, você colocaria <Outlet /> ou <router-view> aqui -->
+      </main>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import menuLateral from "@/components/menuLateral.vue";
+import logo from "@/components/logo.vue";
+import navBar from "@/components/navBar.vue";
+import { onMounted, ref } from "vue";
+
+const sidebarAberto = ref(true);
+
+onMounted(() => {});
 </script>
 
 <style lang="scss" scoped></style>
